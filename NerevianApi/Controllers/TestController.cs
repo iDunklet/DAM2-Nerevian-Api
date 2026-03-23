c48841e9d7b6b9f1519fe1b0b4046aa8c1c5ac13
IDunklet <dunkletodeus@gmail.com>
2026-03-23 16:12:15 +0100

    Agregar archivos de proyecto.

---
+++ NerevianApi/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NerevianApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetGreeting() { }
+        
+    }
+}
