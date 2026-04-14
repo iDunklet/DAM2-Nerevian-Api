c752b4d440c9540de6c7db957c9140860d8398ee
Chenhan <cxiang2425@politecnics.barcelona>
2026-04-14 23:59:25 +0200

    tracking  ETA controlador

---
+++ NerevianApi/Controllers/TrackingETAController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NerevianApi.Models; 
+
+namespace NerevianApi.Controllers
+    {
+        [Route("api/[controller]")] 
+        [ApiController]
+        public class TrackingETAController : ControllerBase
+        {
+            [HttpGet("{id}")]
+            public ActionResult<TrackingETAResponse> GetTrackingInfo(string id)
+            {
+                if (id == "EXP-8822")
+                {
+                    return Ok(new TrackingETAResponse
+                    {
+                        ReferenceCode = "EXP-8822",
+                        Route = "NGB -> ITA",
+                        EtaDate = "16 Oct 2013",
+                        GlobalStatus = "En Aduanas",
+                        ContainerNumber = "CMAU7766552",
+
+                        History = new List<TrackingStatusItem>
+                        {
+                            new TrackingStatusItem { Title = "almacén", Time = "26 Sep 2023, 09:00", IsCompleted = true },
+                            new TrackingStatusItem { Title = "Despacho", Time = "28 Sep 2023, 16:45", IsCompleted = true },
+                            new TrackingStatusItem { Title = "Salida", Time = "01 Oct 2023, 10:15", IsCompleted = true },
+                            new TrackingStatusItem { Title = "En tránsito marítimo", Time = "05 Oct 2023, 08:00", IsCompleted = false },
+                            new TrackingStatusItem { Title = "Llegada a puerto", Time = "12 Oct 2023, 14:30", IsCompleted = false }
+                        }
+
+                    });
+                }
+                else if (id == "EXP-8823")
+                {
+                    return Ok(new TrackingETAResponse
+                    {
+                        ReferenceCode = "EXP-8823",
+                        Route = "QIN -> MAD",
+                        EtaDate = "20 Oct 2028",
+                        GlobalStatus = "Pendiente", 
+                        ContainerNumber = "MEDU3344553"
+                    });
+                }
+                else
+                {
+                    return Ok(new TrackingETAResponse
+                    {
+                        ReferenceCode = "EXP-8821",
+                        Route = "SHA -> VLC",
+                        EtaDate = "13 Oct 2032",
+                        GlobalStatus = "En Tránsito",
+                        ContainerNumber = "MSKU9988771"
+                    });
+                }
+            }
+        }
+    }
---
+++ NerevianApi/Controllers/TrackingETAResponse.cs
@@ -0,0 +1,21 @@
+﻿namespace NerevianApi.Models 
+{
+
+    public class TrackingStatusItem
+    {
+        public string Title { get; set; }
+        public string Time { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+    public class TrackingETAResponse
+    {
+        public string ReferenceCode { get; set; }
+        public string Route { get; set; }
+        public string EtaDate { get; set; }
+        public string GlobalStatus { get; set; }
+        public string ContainerNumber { get; set; }
+
+        public List<TrackingStatusItem> History { get; set; }
+    }
+
+}
